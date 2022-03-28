1748fb62e307d977621867b2bff4abc6e5a00876
Smarty2609 <saini.shivam9286@gmail.com>
2022-03-28 23:22:26 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
+import list from './csvjson.json'
+import React, {useState} from 'react';
 import './App.css';
-
+// gaurav@kuants.in
+// ayush@kuants.in
 function App() {
+ 
+  const[posts,setPosts] = useState(list);
+
+const [ inputData, setInput] = React.useState("");
+const replica =list;
+
+
+const handleSearch = (e) =>{
+  setInput(e.target.value);
+  filteredItems(e.target.value);
+};
+
+const filteredItems = (searchTweet)=> {
+   let searchtweet = searchTweet.toLowerCase();
+  let  filteredList =  replica.filter((item)=>{
+      return item.tweet.toLowerCase().includes(searchtweet);
+    })
+    setPosts(filteredList)   
+  }
+  
+  
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type="text" placeholder='Search here..' value={inputData} onChange={handleSearch}></input>
+      <ul>
+        {inputData && posts.map((items)=>{ 
+          return  <li key={items.id}>{items.tweet}</li>
+        })} 
+      </ul>
     </div>
   );
 }
